269a9934737c5942694756902b173a5bc01da72f
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-10 18:44:43 +0900

    Basic UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import OpenAI from "openai";
+import { useState } from "react";
+
+function generate(options: { apiKey: string; prompt: string }) {
+  const openai = new OpenAI({
+    apiKey: options.apiKey,
+  });
+  console.log("TODO: generate design from prompt");
+}
+
 export default function Home() {
-  return <main>TODO</main>;
+  const [apiKey, setApiKey] = useState("");
+  const [prompt, setPrompt] = useState("");
+
+  return (
+    <main>
+      <div className="border border-gray-200 p-4 rounded-xl">
+        <label>OpenAI API Key</label>
+        <input
+          className="border border-gray-200 p-2 w-full"
+          type="text"
+          value={apiKey}
+          onChange={(event) => setApiKey(event.target.value)}
+        />
+      </div>
+      <div>
+        <textarea
+          className="border border-gray-200 p-4 w-full rounded-xl"
+          value={prompt}
+          placeholder="Enter a prompt to generate a design."
+          onChange={(event) => setPrompt(event.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white p-2 rounded-xl"
+          onClick={() => {
+            generate({
+              apiKey,
+              prompt,
+            });
+          }}
+        >
+          Generate
+        </button>
+      </div>
+    </main>
+  );
 }
