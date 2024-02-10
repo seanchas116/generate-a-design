3588f8a1282801087964594868a25dcdefc15cf1
Ryohei Ikegami <iofg2100@gmail.com>
2024-02-10 23:04:15 +0900

    Refactor with MobX

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import OpenAI from "openai";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Icon } from "@iconify/react";
 import {
   Dialog,
@@ -16,6 +16,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import dedent from "dedent";
+import { observable, makeObservable, runInAction } from "mobx";
+import { observer } from "mobx-react-lite";
 
 class Preferences {
   constructor() {}
@@ -31,57 +33,78 @@ class Preferences {
 
 const preferences = new Preferences();
 
-async function generate(options: { apiKey: string; prompt: string }) {
-  const openai = new OpenAI({
-    apiKey: options.apiKey,
-    dangerouslyAllowBrowser: true,
-  });
+const systemMessage: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
+  role: "system",
+  content: dedent`You are a very talented Web designer from 2023.
+    Generate a modern fancy beautiful UI design for user input.
+    
+    In the first response, generate the outline of the website content.
+    
+    In the second response, generate the wireframe in HTML code.
+    
+    In the third response, generate the final take of the HTML code.
+    
+    Use as much image as possible. <img> src attributes should be placeholder image links.
+    
+    Output format: HTML with Tailwind classes
+    Output without <html> and <body> tags.
+  `,
+};
 
-  const stream = await openai.chat.completions.create({
-    model: "gpt-4-turbo-preview",
-    messages: [
-      {
-        role: "system",
-        content: dedent`You are a very talented Web designer from 2023.
-          Generate a modern fancy beautiful UI design for user input.
-          
-          In the first response, generate the outline of the website content.
-          
-          In the second response, generate the wireframe in HTML code.
-          
-          In the third response, generate the final take of the HTML code.
-          
-          Use as much image as possible. <img> src attributes should be placeholder image links.
-          
-          Output format: HTML with Tailwind classes
-          Output without <html> and <body> tags.
-        `,
-      },
-      {
-        role: "user",
-        content: options.prompt,
-      },
-    ],
-    stream: true,
-  });
-
-  const chunks: string[] = [];
-
-  for await (const chunk of stream) {
-    chunks.push(chunk.choices[0]?.delta?.content || "");
+class ChatGenerator {
+  constructor() {
+    makeObservable(this);
   }
 
-  return chunks.join("");
+  @observable _apiKey = preferences.apiKey;
+  @observable result = "";
+
+  get apiKey() {
+    return this._apiKey;
+  }
+
+  set apiKey(apiKey: string) {
+    this._apiKey = apiKey;
+    preferences.apiKey = apiKey;
+  }
+
+  async generate(prompt: string) {
+    const openai = new OpenAI({
+      apiKey: this.apiKey,
+      dangerouslyAllowBrowser: true,
+    });
+
+    const stream = await openai.chat.completions.create({
+      model: "gpt-4-turbo-preview",
+      messages: [
+        systemMessage,
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      stream: true,
+    });
+
+    const chunks: string[] = [];
+
+    for await (const chunk of stream) {
+      chunks.push(chunk.choices[0]?.delta?.content || "");
+    }
+
+    runInAction(() => {
+      this.result = chunks.join("");
+    });
+  }
 }
 
-export default function Home() {
-  const [apiKey, setApiKey] = useState(preferences.apiKey);
+const Home = observer(function Home() {
   const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
+  const generator = useMemo(() => new ChatGenerator(), []);
 
   return (
     <main className="text-gray-800">
-      <div className="whitespace-pre-wrap">{result}</div>
+      <div className="whitespace-pre-wrap">{generator.result}</div>
       <div className="fixed bottom-4 left-0 right-0 mx-auto w-[640px] h-32 rounded-2xl shadow-lg">
         <textarea
           className="absolute w-full h-full border border-gray-300 p-4 rounded-2xl resize-none"
@@ -108,11 +131,10 @@ export default function Home() {
                   <Input
                     className="col-span-3"
                     type="text"
-                    value={apiKey}
+                    value={generator.apiKey}
                     onChange={(event) => {
                       const apiKey = event.target.value;
-                      preferences.apiKey = apiKey;
-                      setApiKey(apiKey);
+                      generator.apiKey = apiKey;
                     }}
                     placeholder="sk-****"
                   />
@@ -128,11 +150,7 @@ export default function Home() {
           <button
             className="bg-blue-500 text-white p-2 rounded-xl"
             onClick={async () => {
-              const result = await generate({
-                apiKey,
-                prompt,
-              });
-              setResult(result);
+              await generator.generate(prompt);
             }}
           >
             Generate
@@ -141,4 +159,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+});
+
+export default Home;
